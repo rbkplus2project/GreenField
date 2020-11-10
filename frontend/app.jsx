37c59b7d5feaa77bfbc545c5f1345d54a9844fb6
Rasha <r.alamleh@gmail.com>
2020-11-10 21:03:45 +0200

    Initialize the component

---
+++ frontend/app.jsx
@@ -0,0 +1,21 @@
+import react from 'react';
+import {render} from 'react-dom';
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+
+    }
+    render() {
+        return (
+
+        );
+    }
+}
+
+ReactDOM.render(<App/> , document.getElementById('app'));
+
+export default App;
