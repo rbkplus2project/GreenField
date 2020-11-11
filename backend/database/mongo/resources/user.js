edcb8882ab9fb50046ce33afbffe9756d9c86ba9
RamiOkkeh <ramiokkeh@gmail.com>
2020-11-11 17:58:14 +0200

    syntax fix

--- backend/database/mongo/resources/user.js
+++ backend/database/mongo/resources/user.js
@@ -1,11 +1,11 @@
 // create user schema
-const mongoose=require('mongoose')
-let userSchema=mongoose.Schema({
-    username:{type:String,unique:true},
-    passoword:String,
-    salt:String
-})
+const mongoose = require('mongoose');
+let userSchema = mongoose.Schema({
+  username: {type: String, unique: true},
+  passoword: String,
+  salt: String
+});
 
-let User=mongoose.model('User',userSchema)
+let User = mongoose.model('User', userSchema);
 
-module.exports=User
+module.exports = User;
