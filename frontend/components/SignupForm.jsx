e53552f149616901ef76fd193aa008fb9b2f093d
Yasir Tobbileh <yasur96@gmail.com>
2020-11-12 00:52:17 +0200

    add submit button functionality

---
+++ frontend/components/SignupForm.jsx
@@ -0,0 +1,73 @@
+import React, { Component } from 'react';
+const axios = require('axios');
+class SignupForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: null,
+            password: null
+        }
+    }
+
+    handleSubmit = (e) => {
+        let options = {
+            url: `http://localhost:3000/${path}`,
+            method: 'post',
+            data: { usdername: this.state.username, password: this.state.password }
+        }
+
+        axios(options)
+            .then((results) => {
+                console.log(results);
+                this.getData();       // what is the received data will be ?
+            })
+
+            .catch((err) => {
+                console.log("error here ====>", err);
+            })
+    }
+
+    getData = () => {
+        let options = {
+            url: `http://localhost:3000/${path}`,
+            method: 'get',
+        }
+
+        axios(options)
+            .then((results) => {
+                console.log(results);
+
+            })
+
+            .catch((err) => {
+                console.log("error here ====>", err);
+            })
+
+    }
+
+    handleChange = (e) => {
+        setState({ [e.target.id]: e.target.value })
+    }
+
+    render() {
+        return (
+            <div>
+                <form id="signIn" onSubmit={this.handleSubmit}>
+                    <h3>Sign In</h3>
+                    
+                    <label htmlFor="username">User Name:</label><br />
+                    <input type="text" id="username" name="username" placeholder="Enter your name" onChange={this.handleChange} /><br />
+
+                    <label htmlFor="Password">Passward:</label><br />
+                    <input type="password" id="password" name="password" placeholder="Enter password photo" onChange={this.handleChange} /><br />
+
+                    <button id="submit">Sign In</button>
+                    <button id="cancel" onClick={this.onTrigger}>Cancel</button>
+                </form>
+            </div>
+        )
+    }
+}
+
+
+export default SignupForm;
