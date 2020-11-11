f7e2ee99a6d8e19ccb51f9ac5fa3d11116019a4b
aya abuyousef <aya.abuyousef@gmail.com>
2020-11-11 15:49:33 +0200

    schema and controller

--- backend/database/mongo/resources/user.js
+++ backend/database/mongo/resources/user.js
@@ -1,1 +1,11 @@
 // create user schema
+const mongoose=require('mongoose')
+let userSchema=mongoose.Schema({
+    username:String,
+    passoword:String,
+    salt:String
+})
+
+let User=mongoose.model('User',userSchema)
+
+module.exports=User
