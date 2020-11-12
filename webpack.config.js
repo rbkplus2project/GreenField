5ea00c808a54374b3b91b304733d668ddc06c2d4
hamdalla2 <hamdalla2@yahoo.com>
2020-11-12 10:47:20 +0200

    merging

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 var path = require('path');
-var SRC_DIR = path.join(__dirname, '/frontend');
+var SRC_DIR = path.join(__dirname, '/frontend/components');
 var DIST_DIR = path.join(__dirname, '/frontend');
 
 module.exports = {
