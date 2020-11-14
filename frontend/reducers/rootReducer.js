29d3e2c1933bfd8aafea91665d6d4b8e23f21b52
Yasir Tobbileh <yasur96@gmail.com>
2020-11-14 08:40:37 +0200

    add a rootReducer

--- frontend/reducers/rootReducer.js
+++ frontend/reducers/rootReducer.js
@@ -1,10 +1,27 @@
 const initState = {
-    username: "ahamd",
-    password: "123"
+    test: [
+        { username: "ahamd", password: "123"}
+    ],
+    register: "",
+    SignIn: ""
 }
 
 const rootReducer = (state = initState, action) => {
+    // console.log("action====>", action);
+    if (action.type === "Show_REGISTER"){
+        return {
+            ...state,
+            register: action.register
+        }
+    }
+    else if (action.type === "Show_SignIn") {
+        return {
+            ...state,
+            SignIn: action.SignIn
+        }
+    }
     return state;
 }
+
 
 export default rootReducer;
