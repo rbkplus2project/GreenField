0d1fbc0f1218659da948fd02eacec8125fd12a19
Yasir Tobbileh <yasur96@gmail.com>
2020-11-14 22:48:32 +0200

    init

---
+++ src/components/GameList.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+class GameList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+    render() {
+        return (
+            <div>
+                <div>
+                    <img src=""></img>
+                </div>
+                <div>
+                    <img src=""></img>
+                </div>
+                <div>
+                    <img src=""></img>
+                </div>
+                <div>
+                    <img src=""></img>
+                </div>
+            </div>
+        )
+    }
+}
+
+
+export default GameList;
