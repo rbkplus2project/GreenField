a317ef37710913e50cc1c920fbaeba26ddf564db
RamiOkkeh <ramiokkeh@gmail.com>
2020-11-14 19:34:16 +0200

    cleaning up

--- backend/database/resources/gameController.js
+++
@@ -1,9 +0,0 @@
-const Game = require('./game');
-
-exports.find = function (req, res) {
-  Game.find(req, res).select('-_id');
-};
-
-// exports.create = function (req, res) {
-//   Game.create(req, res);
-// };
