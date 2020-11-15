5360939af09a046eac98a5b89553c86f8531ae9a
Hamdalla2 <hamdalla2@yahoo.com>
2020-11-15 14:30:00 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/app.js';
+import App from './components/App.js';
 import { Provider} from 'react-redux';
 import {createStore} from 'redux';
 import rootReducer from './reducers/rootReducer.js';
