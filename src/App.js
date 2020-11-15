264ca46b03aaa9df016a0af6cb4cb8b86f9b2868
Yasir Tobbileh <yasur96@gmail.com>
2020-11-15 11:31:45 +0200

    adding footer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+// import { Route, BrowserRouter } from 'react-router-dom';
+import NavBar from './components/NavBar';
 import SignInForm from './components/SignInForm';
 import SignUpForm from './components/SignUpForm';
-import NavBar from './components/NavBar';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
-// import { Route, BrowserRouter } from 'react-router-dom';
+import GameView from './components/GameView';
+import GameList from './components/GameList';
+import Footer from './components/Footer';
 
-function App() {
-  return (
-    <Router>
-      <div>
-        <NavBar />
-        <Switch>
-          {/* <Route path="/" exact component={NavBar} /> */}
-          <Route path="/signup" component={SignUpForm} />
-          <Route path="/login" component={SignInForm} /> 
-        </Switch> 
-      </div>
-    </Router>
-  );
+
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <NavBar />
+          <Switch>
+            {/* <Route path="/" exact component={NavBar} /> */}
+            <Route path="/signup" component={SignUpForm} />
+            <Route path="/login" component={SignInForm} />
+          </Switch>
+          {/* <GameView /> */}
+          <GameList />
+          <Footer />
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
