005244f87e3a4dcb39f07085017330b1a9dbed88
Yasir Tobbileh <yasur96@gmail.com>
2020-11-15 19:35:32 +0200

    define action display or hide the navbar menu

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,18 +1,6 @@
-export const SignUp = (x) => {
-    return {
-        type: 'Show_SignUp',
-        SignUp: x,
-    }
-}
-export const SignIn = (y) => {
-    return {
-        type: 'Show_SignIn',
-        SignIn: y
-    }
-}
 export const showMenu = (z) => {
     return {
-        type: 'Show_MENU',
+        type: 'SHOW_MENU',
         showMenu: z
     }
 }
