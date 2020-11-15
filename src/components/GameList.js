166d5fce8a8d4a885bafbc7d6248ff347eb51b0c
Rasha <r.alamleh@gmail.com>
2020-11-15 15:34:14 +0200

    Game Show and Game List

--- src/components/GameList.js
+++ src/components/GameList.js
@@ -1,30 +1,43 @@
 import React, { Component } from 'react';
+import '../index.css';
+
+const game1 = [
+    "https://image.shutterstock.com/image-vector/memory-game-preschool-children-vector-260nw-1200034981.jpg",
+    "https://image.shutterstock.com/image-vector/memory-game-preschool-children-vector-600w-531224158.jpg",
+    "https://image.shutterstock.com/image-vector/memory-game-children-cards-transport-600w-632975588.jpg"
+];
+const game2 = [];
+
+
+
+
 class GameList extends Component {
     constructor(props) {
         super(props);
         this.state = {
-
         }
+    }
+    handleClick = (e) => {
     }
     render() {
         return (
-            <div>
-                <div>
-                    <img alt="" src=""></img>
+            <div className="games-container">
+                <div className="games-item">
+                    <img src="https://picsum.photos/400/300" alt="" onClick={this.handleClick}></img>
+                    
+
+                </div >
+                <div className="games-item">
+                    <img src="https://picsum.photos/400/300" alt="" onClick={this.handleClick}></img>
                 </div>
-                <div>
-                    <img alt="" src=""></img>
+                <div className="games-item">
+                    <img src="https://picsum.photos/400/300" alt="" onClick={this.handleClick}></img>
                 </div>
-                <div>
-                    <img alt="" src=""></img>
-                </div>
-                <div>
-                    <img alt="" src=""></img>
+                <div className="games-item">
+                    <img src="https://picsum.photos/400/300" alt="" onClick={this.handleClick}></img>
                 </div>
             </div>
         )
     }
 }
-
-
 export default GameList;
