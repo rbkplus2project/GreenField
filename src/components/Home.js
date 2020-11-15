a563f5035f3bab9947c3440c3799e46d793fc851
hamdalla2 <hamdalla2@yahoo.com>
2020-11-15 19:54:59 +0200

    home

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import GameList from './GameList'
+import GameView from './GameView'
+let Home = () => {
+    return (
+        <div>
+            <GameView />
+            <GameList />
+        </div>
+    )
+}
+export default Home
