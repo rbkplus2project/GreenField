e5e1c432eccab7df1998361275a298a723eea22d
Yasir Tobbileh <yasur96@gmail.com>
2020-11-16 14:20:54 +0200

    refactoring navbar component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -15,7 +15,6 @@ class NavBar extends Component {
     }
   }
   handleSearch = () => {
-    // console.log(this.props)
     if (this.props.showSearch === "hide") {
       this.props.search("show");
     }
@@ -25,69 +24,55 @@ class NavBar extends Component {
   }
 
   render() {
-    console.log(localStorage)
-    if (localStorage.getItem('gamesio')) {
-      return (
-        <div className="menu" >
-          <img className="navitem" alt="" src="./media/rightarrow.png" onClick={this.handleClick} style={{ display: this.props.showMenu === "show" ? "none" : "" }}></img>
+    return (
+      <div className="menu" >
+        <img className="navitem" alt="" src="./media/rightarrow.png" onClick={this.handleClick} style={{ display: this.props.showMenu === "show" ? "none" : "" }}></img>
 
-          <nav id="img-logo" className="row" style={{ display: this.props.showMenu === "show" ? "" : "none" }} >
+        <nav id="img-logo" className="row" style={{ display: this.props.showMenu === "show" ? "" : "none" }} >
 
-            <img className="navitem" alt="" src="./media/leftarrow.png" onClick={this.handleClick}></img>
+          <img className="navitem" alt="" src="./media/leftarrow.png" onClick={this.handleClick}></img>
 
-            <input type="image" className="navitem" alt="Search" src="./media/search.png" onClick={this.handleSearch}></input>
-            <input type="serach" className="seach-input" placeholder="Seacrh..." style={{ display: this.props.showSearch === "show" ? "" : "none" }} ></input>
+          <input type="image" className="navitem" alt="Search" src="./media/search.png" onClick={this.handleSearch}></input>
+          <input type="serach" className="seach-input" placeholder="Seacrh..." style={{ display: this.props.showSearch === "show" ? "" : "none" }} ></input>
 
-            <Link to="/">
-              <input type="image" className="navitem" alt="Home" src="./media/house.png"></input>
-            </Link ><Link to="/signout">
-              <input type="image" className="navitem" alt="SignOut" src="./media/signout.png"></input>
-            </Link><Link to="/settings">
-              <input type="image" className="navitem" alt="Settings" src="./media/cog.png"></input>
-            </Link>
+          <Link to="/">
+            <input type="image" className="navitem" alt="Home" src="./media/house.png"></input>
+          </Link >
 
-          </nav>
-        </div>
-      )
-    } else {
-      return (
-        <div className="menu" >
-          <img className="navitem" alt="" src="./media/rightarrow.png" onClick={this.handleClick} style={{ display: this.props.showMenu === "show" ? "none" : "" }}></img>
+          <Link to="/signin">
+            <input type="image" className="navitem" alt="SignIn" style={{ display: this.props.showSign === "show" ? "none" : "" }} src="./media/signin.png"></input>
+          </Link>
 
-          <nav id="img-logo" className="row" style={{ display: this.props.showMenu === "show" ? "" : "none" }} >
-            
-            <img className="navitem" alt="" src="./media/leftarrow.png" onClick={this.handleClick}></img>
+          <Link to="/signup">
+            <input type="image" className="navitem" alt="SignUp" style={{ display: this.props.showSign === "show" ? "none" : "" }} src="./media/signup.png"></input>
+          </Link>
 
-            <input type="image" className="navitem" alt="Search" src="./media/search.png" onClick={this.handleSearch}></input>
-            <input type="serach" className="seach-input" placeholder="Seacrh..." style={{ display: this.props.showSearch === "show" ? "" : "none" }} ></input>
+          <Link to="/signout">
+            <input type="image" className="navitem" alt="SignOut" style={{ display: this.props.showSign === "show" ? "" : "none" }} src="./media/signout.png"></input>
+          </Link>
 
-            <Link to="/">
-              <input type="image" className="navitem" alt="Home" src="./media/house.png"></input>
-            </Link ><Link to="/signin">
-              <input type="image" className="navitem" alt="SignIn" src="./media/signin.png"></input>
-            </Link><Link to="/signup">
-              <input type="image" className="navitem" alt="SignUp" src="./media/signup.png"></input>
-            </Link><Link to="/settings">
-              <input type="image" className="navitem" alt="Settings" src="./media/cog.png"></input>
-            </Link>
+          <Link to="/settings">
+            <input type="image" className="navitem" alt="Settings" src="./media/cog.png"></input>
+          </Link>
 
-          </nav>
-        </div>
-      );
-    }
+        </nav>
+      </div>
+    );
+    // }
   }
 }
 
 const mapStateToProps = (state) => {
   return {
     showMenu: state.showMenu,
-    showSearch: state.showSearch
+    showSearch: state.showSearch,
+    showSign: state.showSign
   }
 }
 const mapDispatchToProps = (dispatch) => {
   return {
     menu: (z) => { dispatch(showMenu(z)) },
-    search: (z) => { dispatch(showSearch(z))}
+    search: (z) => { dispatch(showSearch(z)) }
   }
 }
 
