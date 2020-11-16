6bc2273ecba40a0710a814e875afa4f854bf405f
RamiOkkeh <ramiokkeh@gmail.com>
2020-11-16 16:26:41 +0200

    refactoring game list

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -4,3 +4,15 @@ export const showMenu = (z) => {
         showMenu: z
     }
 }
+export const getGames = (z) => {
+    return {
+        type: 'SHOW_GAMES',
+        Games: z
+    }
+}
+export const saveGameIndex = (z) => {
+    return {
+        type: 'SAVE_GAME_INDEX',
+        GameIndex: z
+    }
+}
--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -2,14 +2,29 @@ const initState = {
     test: [
         { username: "ahamd", password: "123"}
     ],
-    showMenu: "hide"
+    showMenu: "hide",
+    Games: [],
+    GameIndex: -1
 }
 
 const rootReducer = (state = initState, action) => {
+    console.log(action)
     if (action.type === "SHOW_MENU") {
         return {
             ...state,
             showMenu: action.showMenu
+        }
+    }
+    if (action.type === "SHOW_GAMES") {
+        return  {
+            ...state,
+            Games: action.Games
+        }
+    }
+    if (action.type === "SAVE_GAME_INDEX") {
+        return  {
+            ...state,
+            GameIndex: action.GameIndex
         }
     }
     return state;
