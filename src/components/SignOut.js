ecc7ccdd0c810a4f9842ad7255049702b9a9d3b9
RamiOkkeh <ramiokkeh@gmail.com>
2020-11-16 14:13:51 +0200

    game frame page

--- src/components/SignOut.js
+++ src/components/SignOut.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Navbar from './NavBar'
 
 let SignOut = () => {
-    let signout = () => { Navbar.setState({}); localStorage.removeItem('gamesio')} 
+    let signout = () => localStorage.removeItem('gamesio') 
     return (
         <div>
             <h2 className="styled">Sign Out?</h2>
