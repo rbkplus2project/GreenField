6b473fba33586040506c52a933d618d24886e721
Yasir Tobbileh <yasur96@gmail.com>
2020-11-17 22:16:45 +0200

    add new feat. to signin, signup

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,7 @@ import SignIn from './SignIn.js';
 import SignUp from './SignUp.js';
 import NavBar from './NavBar.js';
 import AddGame from './AddGame.js';
+import ResetPassword from './ResetPassword.js';
 import Home from './Home';
 
 class App extends Component {
@@ -22,6 +23,7 @@ class App extends Component {
                     <Route path="/signout" component={SignOut}/>
                     <Route path="/frame" component={GameFrame}/>
                     <Route path="/addgame" component={AddGame}/>
+                    <Route path="/reset" component={ResetPassword} />
                 </Switch>
             </div>
             </Router>
