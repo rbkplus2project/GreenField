493efb66be00105ce0ed457993513253c4ab91c0
Yasir Tobbileh <yasur96@gmail.com>
2020-11-18 22:57:12 +0200

    update the user schema

--- server/database/resources/user.js
+++ server/database/resources/user.js
@@ -1,15 +1,46 @@
 // create user schema
 const mongoose = require('mongoose');
-let userSchema = mongoose.Schema({
-  username: {type: String, unique: true},
-  email: { type: String, unique: true },
-  password: {rype: String},
-  salt: String,
+const { isEmail } = require('validator');
+const bcrypt = require('bcrypt');
+
+let userSchema = new mongoose.Schema({
+  username: { 
+    type: String, 
+    unique: true, 
+    required: true 
+  },
+  email: { 
+    type: String, 
+    unique: true, 
+    lowercase: true, 
+    required: [true, 'Please enter an email'],
+    validate: [isEmail, 'Please enter a valid email']
+  },
+  password: { 
+    type: String, 
+    required: [true, 'Please enter a password'],
+    minlength: [5, 'Minimum password length is 5 characters'],
+  },  
+  // salt: String,
   games: Array,
   resetPasswordToken: String,
   resetPasswordExpires: Date
 });
 
-let User = mongoose.model('User', userSchema);
+// userSchema.statics.login = async function (username, password) {
+//   const user = await this.findOne({ username });
+//   console.log("+++++++>>",user)
+//   if (user) {
+//     const auth = await bcrypt.compare(password, user.password);
+//     console.log(auth)
+//     if (auth) {
+//       return user;
+//     }
+//     throw Error('incorrect password');
+//   }
+//   throw Error('incorrect email');
+// };
+
+let User = mongoose.model('user', userSchema);
 
 module.exports = User;
