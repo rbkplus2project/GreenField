318fd4f587ef48b5926031dc61aba4ce651f4bc2
hamdalla2 <hamdalla2@yahoo.com>
2020-11-19 23:31:24 +0200

    colors

--- src/components/GameFrame.js
+++ src/components/GameFrame.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
-import {connect} from 'react-redux'
+import {connect} from 'react-redux';
+import $ from 'jquery';
 
 class GameFrame extends Component {
     constructor(props) {
@@ -8,7 +9,12 @@ class GameFrame extends Component {
         this.state = {
         }
     }    
-    
+    componentDidMount(){
+        $("#particles")[0].className="hide"
+    }
+    componentWillUnmount(){
+        $("#particles")[0].className="Show"
+    }
     render() {
     if (localStorage.getItem('gamesio') && this.props.gameIndex !== -1) {
         return (
