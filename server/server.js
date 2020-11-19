155c6f6cc3cd003d0ab6df6d6ce4da06e27de71b
RamiOkkeh <ramiokkeh@gmail.com>
2020-11-19 17:08:55 +0200

    still heroku deploying

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
-const mongo = require('./server/database');
+const mongo = require('./database');
 
 let app = express();
 
@@ -12,9 +12,9 @@ app.use(express.static(__dirname + '/build'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/user/signup', require('./server/middleware/hash.js'));
-app.use('/user', require('./server/database/resources/userRouter'));
-app.use('/game', require('./server/database/resources/gameRouter'));
+app.use('/user/signup', require('./middleware/hash.js'));
+app.use('/user', require('./database/resources/userRouter'));
+app.use('/game', require('./database/resources/gameRouter'));
 
 app.get('/*', (req, res) => {
   res.sendFile(path.resolve(__dirname, "./build", "index.html"))
@@ -24,7 +24,7 @@ const host = '0.0.0.0';
 const port = process.env.PORT || 3000;
 
 app.listen(port, host, () => {
-  console.log('Listening on port: ' + process.env.PORT);
+  console.log('Listening on port: ' + port);
 });
 
 module.exports = app;
