f61013a702b6e291fb2ee82e7b0885458a642759
hamdalla2 <hamdalla2@yahoo.com>
2020-11-21 11:07:44 +0200

    coloring

--- src/components/Color.js
+++ src/components/Color.js
@@ -1,40 +1,46 @@
 import React, { Component } from 'react';
 import $ from 'jquery';
 
-
 class Color extends Component {
   containers(){
-    $(".i").style.backgroundColor=""
+    
   }
   borders(){
-    $(".i").style.backgroundColor=""
+    
   }
   menu(){
-    $(".i").style.backgroundColor=""
+    
   }
   font(){
-    $(".i").style.backgroundColor=""
+    ($('.button').css({color:$("#c4").val()}))
+  }
+  hover(){
+
   }
   background(){
-    $(".i").style.backgroundColor=""
+    
   }
   particles(){
-    $(".i").style.backgroundColor=""
+    
   }
   links(){
-    $(".i").style.backgroundColor=""
+    
   }
     render() {
         return (
           <div className="center column styled">
-            <div>Containers Color : <input type="color" onChange={this.containers} /></div>
-            <div>Borders Color : <input type="color" onChange={this.borders} /></div>
-            <div>Menu Color : <input type="color" onChange={this.menu} /></div>
-            <div>Font Color : <input type="color" onChange={this.font} /></div>
+            <div>Containers Color : <input type="color" id="c1" onChange={this.containers} /></div>
+            <div>Borders Color : <input type="color" id="c2" onChange={this.borders} /></div>
+            <div>Menu Color : <input type="color" id="c3" onChange={this.menu} /></div>
+            <div>Font Color : <input type="color" id="c4" onChange={this.font} /></div>
+            <div>Hover : <input type="color" id="c5" onChange={this.hover} /></div>
              <br/>  
-            <div>Background Color : <input type="color" onChange={this.background} /></div>
-            <div>Particles Color : <input type="color" onChange={this.particles} /></div>
-            <div>Links Color : <input type="color" onChange={this.links} /></div>
+            <div>Background Color : <input type="color" id="c6" onChange={this.background} /></div>
+            <div>Particles Color : <input type="color" id="c7" onChange={this.particles} /></div>
+            <div>Links Color : <input type="color" id="c9" onChange={this.links} /></div>
+            <div className="button">H
+            </div>
+
           </div>
         )
     }
