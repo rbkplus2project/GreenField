e4cce78ef09ee79804c6966f3bc5dedb662ca956
RamiOkkeh <ramiokkeh@gmail.com>
2020-11-21 11:11:51 +0200

    static.json (still heroku setup)

--- server/server.js
+++ server/server.js
@@ -23,7 +23,7 @@ app.use('/game', require('./database/resources/gameRouter'));
 // Responds to all get requests
 app.get('/*', (req, res) => {
   console.log('asdfghjklkjhgfdsasdfghjk -------------------> ', __dirname)
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../build', 'index.html'));
 })
 
 const host = '0.0.0.0';
